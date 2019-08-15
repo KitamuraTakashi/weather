328388f0d669b27de6d2afac88f7c3d455bdb634
takashikitamura <monkeydolce@gmail.com>
2019-08-15 12:42:25 +0900

    Initial commit

---
+++ src/database/migrations/2019_08_15_033009_create_weathers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeathersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weathers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('pinpoint_code');
+            $table->dateTime('data_time');
+            $table->string('weather_code');
+            $table->string('weather_name');
+            $table->integer('temperature_min');
+            $table->integer('temperature_max');
+            $table->integer('rain_percentage');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weathers');
+    }
+}
